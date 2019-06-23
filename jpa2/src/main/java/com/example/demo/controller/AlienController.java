962c34fb88ac18b8d87331770549d4a58cf0a2c9
Manasa <manasa.navuduri@gmail.com>
2019-06-23 17:18:54 +0530

    Retreive

--- jpa2/src/main/java/com/example/demo/controller/AlienController.java
+++ jpa2/src/main/java/com/example/demo/controller/AlienController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
@@ -23,12 +25,12 @@ public String addAlien(Alien alien) {
 		repo.save(alien);
 	return "home.jsp";
       }
-	@RequestMapping("/getAlien")
-	public ModelAndView getAlien(@RequestParam int aid) {
-		ModelAndView mv=new ModelAndView("showAlien.jsp");
-		Alien alien=repo.findById(aid).orElse(new Alien());
-		mv.addObject(alien);
-		return mv;
+	
+	@RequestMapping("/alien/{aid}")
+	@ResponseBody
+	public String getAlien(@PathVariable("aid") int aid)
+	{
+		return repo.findById(aid).toString();
 	}
 	
 		}
--- jpa2/src/main/resources/data.sql
+++ jpa2/src/main/resources/data.sql
@@ -1,1 +1,5 @@
 insert into alien values(101,'Maani');
+insert into alien values(102,'Manasa');
+insert into alien values(103,'Neeha');
+insert into alien values(104,'Simran');
+insert into alien values(105,'Nithya');

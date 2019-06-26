8988aedd6a9f958fa01b983b36b8256e5b2b0d7e
Manasa <manasa.navuduri@gmail.com>
2019-06-26 13:29:53 +0530

    MVC

--- jpa2/src/main/java/com/example/demo/controller/AlienController.java
+++ jpa2/src/main/java/com/example/demo/controller/AlienController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,9 +36,9 @@ public String addAlien(Alien alien) {
 	
 	@RequestMapping("/alien/{aid}")
 	@ResponseBody
-	public String getAlien(@PathVariable("aid") int aid)
+	public Optional<Alien> getAlien(@PathVariable("aid") int aid)
 	{
-		return repo.findById(aid).toString();
+		return repo.findById(aid);
 	}
 	
 		}
